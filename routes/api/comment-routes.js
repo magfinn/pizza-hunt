d229f6ee9ee369ecbf58b39d420d5828450e09f8
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-02-05 18:02:55 -0800

    updated comment functionality to include replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require('../../controllers/comment-controller');
 
 //add comment
@@ -10,6 +12,10 @@ router.route('/:pizzaId').post(addComment);
 
 //remove comment
 //DELETE /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+
+//remove reply
+// /:pizzaId/:commentId/:replyId
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
